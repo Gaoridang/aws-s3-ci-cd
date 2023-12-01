1860608a67eb26bf734b8632bccefdbbd60a1634
Gaoridang <oingc@naver.com>
2023-12-02 00:45:39 +0900

    Feat: 본문 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ function App() {
   return (
     <div className=' flex flex-col gap-2 p-4 justify-center items-center h-full border-orange-700 border-[80px]'>
       <h1 className=' text-4xl text-black font-extrabold'>Hello World</h1>
-      <p className=' text-center'>This is AWS S3 and CloudFront CI/CD project</p>
+      <p className=' text-center'>
+        This is AWS S3 and CloudFront deploy project with github actions CI/CD
+      </p>
     </div>
   );
 }
