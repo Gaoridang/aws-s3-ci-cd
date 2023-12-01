a924af6615f231fbb50f5810eacf538226e5fbb5
gaori <monooi@naver.com>
2023-12-01 15:39:50 +0900

    Design: 디자인 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <></>;
+  return (
+    <div className=" flex flex-col gap-2 justify-center items-center h-full border-slate-700 border-[80px]">
+      <h1 className=" text-4xl text-black font-extrabold">Hello World</h1>
+      <p>This is AWS S3 and CloudFront CI/CD project</p>
+    </div>
+  );
 }
 
 export default App;
