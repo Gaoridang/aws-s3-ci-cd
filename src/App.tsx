209c009711ac606640b7f4deba1400f5bebf9069
gaori <monooi@naver.com>
2023-12-01 23:26:08 +0900

    Design: 테두리 색 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className=" flex flex-col gap-2 justify-center items-center h-full border-slate-700 border-[80px]">
-      <h1 className=" text-4xl text-black font-extrabold">Hello World</h1>
+    <div className=' flex flex-col gap-2 justify-center items-center h-full border-orange-700 border-[80px]'>
+      <h1 className=' text-4xl text-black font-extrabold'>Hello World</h1>
       <p>This is AWS S3 and CloudFront CI/CD project</p>
     </div>
   );
