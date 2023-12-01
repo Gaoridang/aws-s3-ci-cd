2668282bb06ebdc0de105cc061ba8dcc26f9fbe4
Gaoridang <oingc@naver.com>
2023-12-02 00:46:41 +0900

    Feat: 노션 링크 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ function App() {
   return (
     <div className=' flex flex-col gap-2 p-4 justify-center items-center h-full border-orange-700 border-[80px]'>
       <h1 className=' text-4xl text-black font-extrabold'>Hello World</h1>
-      <p className=' text-center'>
+      <p className=' text-center mb-2'>
         This is AWS S3 and CloudFront deploy project with github actions CI/CD
       </p>
+      <a href='https://www.notion.so/AWS-S3-CI-CD-f7084cd5ab094485a2ebb86e05df951a'>
+        Go to Notion for more information
+      </a>
     </div>
   );
 }
