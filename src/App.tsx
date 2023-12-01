1511b782ab2776087884b7dc405cc2ad17f27ef9
Gaoridang <oingc@naver.com>
2023-12-02 00:29:15 +0900

    Design: 본문 중앙 정렬

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className=' flex flex-col gap-2 p-4 justify-center items-center h-full border-orange-700 border-[80px]'>
       <h1 className=' text-4xl text-black font-extrabold'>Hello World</h1>
-      <p>This is AWS S3 and CloudFront CI/CD project</p>
+      <p className=' text-center'>This is AWS S3 and CloudFront CI/CD project</p>
     </div>
   );
 }
