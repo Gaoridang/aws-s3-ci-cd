9f784fee7d7aa71b0ae0e6797ee83a1addeee7f0
gaori <monooi@naver.com>
2023-12-02 00:27:05 +0900

    Design: padding 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className=' flex flex-col gap-2 justify-center items-center h-full border-orange-700 border-[80px]'>
+    <div className=' flex flex-col gap-2 p-4 justify-center items-center h-full border-orange-700 border-[80px]'>
       <h1 className=' text-4xl text-black font-extrabold'>Hello World</h1>
       <p>This is AWS S3 and CloudFront CI/CD project</p>
     </div>
