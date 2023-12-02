c823c7fe7cd375c7cb72dbf5338deb8f85dd4df6
gaori <monooi@naver.com>
2023-12-02 22:31:56 +0900

    Feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import AwsS3 from './shared/aws';
+import LinkEl from './shared/components/Link/Link';
 
 function App() {
-  return <AwsS3 />;
+  return (
+    <div className='flex flex-col gap-2 border-[44px] border-green-700 h-full p-8'>
+      <h1 className=' font-extrabold text-4xl '>PROJECTS</h1>
+      <LinkEl to='/aws-s3-ci-cd'>AWS S3 and CloudFront with Github Actions CI/CD</LinkEl>
+      <LinkEl to='/suspense-error-boundary'>Suspense and Error Boundary</LinkEl>
+    </div>
+  );
 }
 
 export default App;
