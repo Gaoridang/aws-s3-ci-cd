2d6fda3d1a813e847766df37004ab2f37009144b
Gaoridang <oingc@naver.com>
2023-12-03 14:59:51 +0900

    Feat: useMovie 훅 제작

---
+++ src/shared/components/SuspenseErrorBoundary/MovieShared/api/index.ts
@@ -0,0 +1,39 @@
+const BASE_URL = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+
+export interface Response<T> {
+  dates: {
+    maximun: string;
+    minimun: string;
+  };
+  page: number;
+  results: T;
+}
+
+type FetchType = <T>(url: string) => Promise<Response<T>>;
+const fetchData: FetchType = async (url) => {
+  const response = await (
+    await fetch(url, {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`,
+      },
+    })
+  ).json();
+  return response.data;
+};
+
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path?: string;
+  release_date: string;
+  title: string;
+}
+
+export const fetchMovies = () => fetchData<Movie[]>(`${BASE_URL}`);
